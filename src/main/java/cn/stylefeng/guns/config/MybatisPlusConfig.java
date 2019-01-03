66ad8de12d23bfc9838f1adaa56dd7f41b1a904c
libo <418431198@qq.com>
2019-01-03 14:00:34 +0800

    01

---
+++ src/main/java/cn/stylefeng/guns/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package cn.stylefeng.guns.config;
+
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.szss.admin.dao.*")
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+}
